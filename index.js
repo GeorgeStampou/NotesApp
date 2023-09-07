47f156640caed8a126542b243d87c6f8bf4f3904
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-09-07 20:44:45 +0300

    delete button added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const notetxt = document.getElementById("noteText");
 const form = document.getElementById("form");
+const notesLst = document.getElementById("notesList");
 
 function onClick(evt){
     evt.preventDefault();
+
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class","libtn");
+    
     let newLi = document.createElement("li");
     newLi.innerText = notetxt.value;
-    document.getElementById("notesList").appendChild(newLi);
+    newDiv.appendChild(newLi);
+
+    const deleteBtn = document.createElement("input");
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.setAttribute("class","btn");
+    deleteBtn.setAttribute("value", "delete");
+    newDiv.appendChild(deleteBtn).addEventListener("click", removeItem);
+
+    notesLst.appendChild(newDiv);
+    
     form.reset();
+}
+
+function removeItem(){
+    this.parentNode.remove();
 }
 
 form.addEventListener("submit", onClick);
